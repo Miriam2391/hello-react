920bb07a6b7049962c9a36f066db798d168f1191
Miriam2391 <mirita2391@gmail.com>
2024-08-22 21:57:15 +0000

    he creado una carpeta Acordion

---
+++ Nuevo/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Acordion from './componentes/Acordion'
+
+
+function App() {
+
+  return (
+    <>
+      <Acordion />
+    </>
+  )
+}
+
+export default App
